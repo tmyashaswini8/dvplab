07440060eef1c69b4bdc3441eac301b58def32d1
tmyashaswini8 <tmyashaswinii@gmail.com>
2024-09-04 00:13:16 -0700

    Add files via upload

---
+++ 1a.py
@@ -0,0 +1,10 @@
+m1=int(input("Enter a number"))
+m2=int(input("Enter a number"))
+m3=int(input("Enter a number"))
+if(m1<m2 and m1<m3):
+    Avg=(m2+m3)/2
+elif(m2<m2 and m2<m3):
+    Avg=(m1+m3)/2
+else:
+    Avg=(m1+m2)/2
+print("Average of two marks:",Avg)
