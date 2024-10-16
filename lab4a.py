027a9daf8bcf35995e879b6da7363035941cf9e3
tmyashaswini8 <tmyashaswinii@gmail.com>
2024-10-16 00:13:08 -0700

    Add files via upload

---
+++ lab4a.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+plt.hist([1,2,3,4,5,6,7,8,9,12,13,14],bins=4)
+plt.xlabel("Distribution of Age",fontsize=16)
+plt.ylabel("Frequency",fontsize=16)
+plt.title("Yashaswini TM-1KI23CS187")
+plt.show()
---
+++ piechart.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+CARS=['KIA','RANGEROVER','MALENO','ALTO','I20','CRETA']
+data=['20','56','67','15','8','65']
+plt.figure(figsize=(10,7))
+plt.pie(data,labels=CARS)
+plt.title("Yashaswini TM=1KI23CS187")
+plt.show()
